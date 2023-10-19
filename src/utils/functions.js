a1320d6e6d13e13350bcbb35ec9ce68acf627f25
if_vitor <127361991+Vitor-VX@users.noreply.github.com>
2023-10-18 21:08:12 -0400

    Update functions.js
    adicionado função para gerenciar o tempo de um login bloqueado

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -7,6 +7,30 @@ const time_LoginBlock_verific = (timeDataBase, timeExpire) => {
     return diferenca_Segundos > timeExpire ? true : false
 }
 
+const systemBlockLogin = async (cliente, timeBlock, operation = 'block') => {
+    switch (operation) {
+        case 'block':
+            const currentTime = new Date()
+            const blockTime = new Date(currentTime.getTime() + timeBlock);
+            cliente.block_login = blockTime;
+            break;
+        case 'unlock':
+            cliente.block_login = null
+            cliente.tentativas = 5
+            break;
+        case 'decrement-tries':
+            cliente.tentativas--;
+            break;
+        case 'reset-tries':
+            cliente.tentativas = 5;
+            break;
+    }
+
+    await cliente.save();
+    return;
+}
+
 module.exports = {
-    time_LoginBlock_verific
+    time_LoginBlock_verific,
+    systemBlockLogin
 }
